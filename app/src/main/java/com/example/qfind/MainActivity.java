cd2c650220106ee04b9b126845e5ebb6aa8d88ea
Ayush Chaudhary <ayush0000ayush@gmail.com>
2022-03-10 04:10:16 +0530

    Reading PDF
    the code read the text of the pdf and prints the read text when the user wants to see the text

---
+++ app/src/main/java/com/example/qfind/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.qfind;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
+
+import java.lang.reflect.Field;
+
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button camera_button;
+    Button select_file_button, addes_file_button;
+    static final int REQUEST_IMAGE_CAPTURE=1;
+
+    // creating variables for
+    // button and text view.
+    private Button extractPDFBtn;
+    private TextView extractedTV;
+    private Button listBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        extractedTV = findViewById(R.id.idPDFTV);
+        extractPDFBtn = findViewById(R.id.idBtnExtract);
+        listBtn = findViewById(R.id.listBtn);
+
+        listBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listRaw();
+            }
+        });
+
+        extractPDFBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                extractPDF();
+            }
+        });}
+        private void extractPDF() {
+            try {
+
+                String extractedText = "";
+
+               PdfReader reader = new PdfReader("/storage/emulated/0/Download/7366a7f1ec0912231deb90e1e086d0eb.pdf");
+               PdfReader reader2 = new PdfReader("res/raw/pdf.pdf");
+
+
+                int n = reader.getNumberOfPages();
+                int n2 = reader2.getNumberOfPages();
+
+                for (int i = 0; i < n; i++) {
+                    extractedText = extractedText + PdfTextExtractor.getTextFromPage(reader, i + 1).trim() + "\n";
+
+                }
+                for (int i = 0; i < n2; i++) {
+                    extractedText = extractedText + PdfTextExtractor.getTextFromPage(reader2, i + 1).trim() + "\n";
+
+                }
+
+                Toast.makeText(MainActivity.this,"now",Toast.LENGTH_SHORT).show();
+
+
+                extractedTV.setText(extractedText);
+
+
+                reader.close();
+            } catch (Exception e) {
+
+                extractedTV.setText("Error found is : \n" + e);
+            }
+        }
+
+
+
+    //this code shows the list of pdfs in the res/raw folder
+    //it will be removed in future
+
+    public void listRaw() {
+        String str= "";
+        Field[] fields = R.raw.class.getFields();
+        for(int count = 0; count < fields.length; count++) {
+
+            str = str + "\n"+ fields[count].getName();
+            extractedTV.setText(str);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/qfind/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.qfind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        imageView= findViewById(R.id.image);
+
+    }
+}
