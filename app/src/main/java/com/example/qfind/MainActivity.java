ddd5a7fd8817f94aa5130a9815b76cfec9b089f5
Ayush Chaudhary <ayush0000ayush@gmail.com>
2022-03-10 18:07:50 +0530

    open the doc from internal storage
    the code now pushed let the user to select the document form the internal of pdf type.

--- app/src/main/java/com/example/qfind/MainActivity.java
+++ app/src/main/java/com/example/qfind/MainActivity.java
@@ -13,10 +13,17 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -42,7 +49,9 @@ public class MainActivity extends AppCompatActivity {
     private Button extractPDFBtn;
     private TextView extractedTV;
     private Button listBtn;
-
+    String path;
+    private Button add_pdf_button;
+    ActivityResultLauncher<String> r_launcher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +61,23 @@ public class MainActivity extends AppCompatActivity {
         extractedTV = findViewById(R.id.idPDFTV);
         extractPDFBtn = findViewById(R.id.idBtnExtract);
         listBtn = findViewById(R.id.listBtn);
+        add_pdf_button=findViewById(R.id.add_pdf);
+
+        r_launcher= registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri uri) {
+                //  final String   path = result.getPath();
+                path = uri.getPath();
+                Toast.makeText(MainActivity.this, "File Selected: " + path, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        add_pdf_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                r_launcher.launch("application/pdf");
+            }
+        });
 
         listBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,14 +92,17 @@ public class MainActivity extends AppCompatActivity {
 
                 extractPDF();
             }
-        });}
-        private void extractPDF() {
+        });
+    }
+
+    private void extractPDF() {
             try {
 
                 String extractedText = "";
 
                PdfReader reader = new PdfReader("/storage/emulated/0/Download/7366a7f1ec0912231deb90e1e086d0eb.pdf");
                PdfReader reader2 = new PdfReader("res/raw/pdf.pdf");
+
 
 
                 int n = reader.getNumberOfPages();
